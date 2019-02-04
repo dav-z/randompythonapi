ca5456e2040d3e318752efd56ba51a61ad99d891
David Zheng <davidzheng@knights.ucf.edu>
2019-02-04 18:14:04 -0500

    recieve messages

--- pai.py
+++ pai.py
@@ -6,77 +6,113 @@ from datetime import date, timedelta
 import requests
 from flask import Flask, request
 from twilio import twiml
+from twilio.twiml.messaging_response import MessagingResponse
+
 
 
 app = Flask(__name__)
 
-
-
-
-## setting the url to the google geocode api to return information in json
-url = 'https://maps.googleapis.com/maps/api/geocode/json'
-
-## setting up parameters (address, API KEY)
-params = {
+@app.route('/sms', methods=['GET', 'POST'])
+def sms():
+    number = request.form['From']
+    message_body = request.form['Body']
+    params = {
     'sensor': 'false',
-    'address': '1208 Frankford Ave, Philadelphia, PA 19125',
+    'address': message_body,
     ## unsafe to store API Key this way, but the repo is private
     'key': 'AIzaSyDWKIU6UoMtxqFFq3R0C8uXAMXTUquwHdM'
     }
 
-## calls request to get the information given from the url with address and api key
-r = requests.get(url, params = params)
+    ## calls request to get the information given from the url with address and api key
+    r = requests.get(url, params = params)
 
-## gets the result in json format
-res = r.json()
+    ## gets the result in json format
+    res = r.json()
 
-## gets first result returned
-result = res['results'][0]
+    ## gets first result returned
+    result = res['results'][0]
 
-## creates a hashmap
-geodata = dict()
+    ## creates a hashmap
+    geodata = dict()
 
-## set variables for long and lat
-lat = result['geometry']['location']['lat']
-lon = result['geometry']['location']['lng']
+    ## set variables for long and lat
+    lat = result['geometry']['location']['lat']
+    lon = result['geometry']['location']['lng']
+    account_sid = "AC0787ce7bbc2aed7293ae60beee27480d"
+    auth_token  = "734e725ff3b749ff21c8ef7a807359af"
+    ## Set up month day format
+    dd = datetime.datetime.today().strftime('%d')
+    mm = datetime.datetime.today().strftime('%m')
+    yy = datetime.datetime.today().strftime('%Y')
 
-## API KEY
-account_sid = "AC0787ce7bbc2aed7293ae60beee27480d"
-auth_token  = "734e725ff3b749ff21c8ef7a807359af"
+    ## Darksky API KEY
+    key = "c9e27dbf8ba59e0820e721f9e3e088f7"
 
-## Set up month day format
-dd = datetime.datetime.today().strftime('%d')
-mm = datetime.datetime.today().strftime('%m')
-yy = datetime.datetime.today().strftime('%Y')
+    location = forecast(key, lat, lon)
 
-## Darksky API KEY
-key = "c9e27dbf8ba59e0820e721f9e3e088f7"
+    testmsg = "Today's temperature is {} degrees at {}".format(location.temperature, message_body)
 
-location = forecast(key, lat, lon)
+    resp = twiml.Response()
+    resp.message(testmsg)
+    return str(resp)
 
-message = "Today's temperature is {} degrees at {}".format(location.temperature, params["address"])
+if __name__ == '__main__':
+    app.run()
 
-
-## Calling twilio API library, using Client
-test = Client(account_sid, auth_token)
-
-## Makes a test message from Twilio # to given #, with a body message of "body"
-sms = test.messages.create(
-    to="+18565059979",
-    from_="+16108549557",
-    body=message)
-
-## Runs the program
-print(sms.sid)
-
-##@app.route('/sms', methods=['POST'])
-##def sms():
-##    number = request.form['From']
-##    message_body = request.form['Body']
 ##
-##    resp = twiml.Response()
-##    resp.message('Hello {}, you said: {}'.format(number, message_body))
-##    return str(resp)
+#### setting the url to the google geocode api to return information in json
+##url = 'https://maps.googleapis.com/maps/api/geocode/json'
 ##
-##if __name__ == '__main__':
-##    app.run()
+#### setting up parameters (address, API KEY)
+##params = {
+##    'sensor': 'false',
+##    'address': message_body,
+##    ## unsafe to store API Key this way, but the repo is private
+##    'key': 'AIzaSyDWKIU6UoMtxqFFq3R0C8uXAMXTUquwHdM'
+##    }
+##
+#### calls request to get the information given from the url with address and api key
+##r = requests.get(url, params = params)
+##
+#### gets the result in json format
+##res = r.json()
+##
+#### gets first result returned
+##result = res['results'][0]
+##
+#### creates a hashmap
+##geodata = dict()
+##
+#### set variables for long and lat
+##lat = result['geometry']['location']['lat']
+##lon = result['geometry']['location']['lng']
+##
+#### API KEY
+##account_sid = "AC0787ce7bbc2aed7293ae60beee27480d"
+##auth_token  = "734e725ff3b749ff21c8ef7a807359af"
+##
+#### Set up month day format
+##dd = datetime.datetime.today().strftime('%d')
+##mm = datetime.datetime.today().strftime('%m')
+##yy = datetime.datetime.today().strftime('%Y')
+##
+#### Darksky API KEY
+##key = "c9e27dbf8ba59e0820e721f9e3e088f7"
+##
+##location = forecast(key, lat, lon)
+##
+##message = "Today's temperature is {} degrees at {}".format(location.temperature, params["address"])
+##
+##
+#### Calling twilio API library, using Client
+##test = Client(account_sid, auth_token)
+##
+#### Makes a test message from Twilio # to given #, with a body message of "body"
+##sms = test.messages.create(
+##    to="+18565059979",
+##    from_="+16108549557",
+##    body=message)
+##
+#### Runs the program
+####print(sms.sid)
+##
